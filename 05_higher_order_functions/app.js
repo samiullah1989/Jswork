1365dc1677cb74c9c980e5c812e5c9850c475f40
Samiullah Khan <samiulah.khan.1989@gmail.com>
2015-06-15 20:01:14 +0300

    start work with JSON

--- 05_higher_order_functions/app.js
+++ 05_higher_order_functions/app.js
@@ -3,3 +3,35 @@ function logEach(array) {
 		console.log(array[i]);
 	}
 }
+
+function logThem(value) {
+	console.log(value);
+}
+
+function forEachArray(array, action) {
+	for (var i = 0; i < array.length; i++) {
+		action(array[i]);
+	}
+}
+
+var numbers = [1,2,3,4,5,6], sum = 0;
+
+forEachArray(numbers, function(number){
+	sum += number;
+});
+
+console.log(sum);
+
+function unless(test, then){
+	if(!test) then();
+}
+
+function repeat(times, body) {
+	for(var i=0; i<times; i++) body(i);
+}
+
+repeat(3, function(n){
+	unless(n % 2, function(){
+		console.log(n, "is even");
+	});
+});
